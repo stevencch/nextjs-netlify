80e8e911319a231f0baa1e03f3a735da99904741
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-07-29 11:19:44 +1000

    'ini'

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+import { ProfileForm } from "@/components/profile-form"
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+      <ProfileForm />
+      </div>
+    </section>
+  )
+}
