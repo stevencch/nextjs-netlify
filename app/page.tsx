571e91fc1224fe6a92ecacf79b5ba650ad37b4ac
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-07-31 21:13:11 +1000

    add contentful

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,73 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { ProfileForm } from "@/components/profile-form"
+import CoverImage from "./cover-image";
+import Avatar from "./avatar";
+import Date from "./date";
+import { getAllPosts } from "@/lib/api";
+import { draftMode } from "next/headers";
+import MoreStories from "./more-stories";
 
-export default function IndexPage() {
+function HeroPost({
+  title,
+  coverImage,
+  date,
+  excerpt,
+  author,
+  slug,
+}: {
+  title: string;
+  coverImage: any;
+  date: string;
+  excerpt: string;
+  author: any;
+  slug: string;
+}) {
+  return (
+    <section>
+      <div className="mb-8 md:mb-16">
+        <CoverImage title={title} slug={slug} url={coverImage.url} />
+      </div>
+      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+        <div>
+          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+            <Link href={`/posts/${slug}`} className="hover:underline">
+              {title}
+            </Link>
+          </h3>
+          <div className="mb-4 md:mb-0 text-lg">
+            <Date dateString={date} />
+          </div>
+        </div>
+        <div>
+          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+          {author && <Avatar name={author.name} picture={author.picture} />}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default async function IndexPage() {
+  const { isEnabled } = draftMode();
+  const allPosts = await getAllPosts(isEnabled);
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="container mx-auto px-5">
+      {heroPost && (
+        <HeroPost
+          title={heroPost.title}
+          coverImage={heroPost.coverImage}
+          date={heroPost.date}
+          author={heroPost.author}
+          slug={heroPost.slug}
+          excerpt={heroPost.excerpt}
+        />
+      )}
+      <MoreStories morePosts={morePosts} />
+    </div>
       <div className="flex max-w-[980px] flex-col items-start gap-2">
       <ProfileForm />
       </div>
